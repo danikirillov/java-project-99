84eb08323e85c79d9205ae4799033d97ca534841
dd <danikirillov@ya.ru>
2025-04-12 14:52:48 +0200

    add profile

--- app/src/main/java/hexlet/code/app/config/LoggingConfig.java
+++ app/src/main/java/hexlet/code/app/config/LoggingConfig.java
@@ -2,9 +2,11 @@ package hexlet.code.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
+@Profile("dev")
 public class LoggingConfig {
 
     @Bean
